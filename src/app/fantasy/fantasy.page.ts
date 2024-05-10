d0e2efe5484ee0960b9ada011a856e61f42f8fa2
MICHAELANGELO OJEANI <G00416140@atu.ie>
2024-05-10 01:07:07 +0100

    Finally finished

--- src/app/fantasy/fantasy.page.ts
+++ src/app/fantasy/fantasy.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,IonGrid,IonRow,IonCard,IonCol,IonCardTitle, IonCardHeader,IonImg,IonCardContent,IonCardSubtitle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-fantasy',
   templateUrl: './fantasy.page.html',
   styleUrls: ['./fantasy.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonGrid,IonRow,IonCard,IonCol,IonCardTitle, IonCardHeader,IonImg,IonCardContent,IonCardSubtitle]
 })
 export class FantasyPage implements OnInit {
 
